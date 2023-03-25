79dd5ec86edc6acf1f3a4d6f25f136063d51eefa
tcggkkk <128909421+tcggkkk@users.noreply.github.com>
2023-03-25 18:44:40 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,120 @@
+<html>
+<head>
+
+<style>
+#redirect {
+  font-family: Arial, Helvetica, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+#redirect td, #redirect th {
+  border: 1px solid #ddd;
+  padding: 8px;
+}
+#redirect tr:nth-child(even){background-color: #f2f2f2;}
+#redirect tr:hover {background-color: #ddd;}
+#redirect th {
+  padding-top: 12px;
+  padding-bottom: 12px;
+  text-align: left;
+  background-color: #04AA6D;
+  color: white;
+}
+</style>
+<meta name="viewport" content= "width=device-width, initial-scale=1.0">
+<title> SPORTS REDIRECT </title>
+</head>
+<body bgcolor = "">
+<a href="#"><img src="https://i.ibb.co/Qrdc1t0/IMG-20230324-100312.jpg" alt="IMG-20230324-100312" width = "110%" border="0"></a>
+<br> <br> NOTE :- ALL THE LINKS MAY NOT WORK! THEY MAY GET START AT MATCH TIME.<br>
+<table id="redirect">
+  <tr>
+    <th>Channel Name</th>
+    <th>Description</th>
+    <th>Play Link</th>
+  </tr>
+  <tr>
+    <td>SKY SPORTS CRICKET</td>
+    <td>Sky Sports Cricket - live scores, news results, highlights, videos, photos, test cricket, and fixtures for International and County Cricket matches.</td>
+    <td><a href = "sportsplayer.php?v=skyscric">Play Link</a></td>
+  </tr>
+  <tr>
+    <td>STAR SPORTS 1</td>
+    <td>Star Sports is a major broadcaster of cricket in India, holding the pay television rights to domestic national team matches, the Indian Premier League, and ...</td>
+    <td><a href = "sportsplayer.php?v=star1in">Play Link</a></td>
+  </tr>
+  <tr>
+    <td>STAR SPORTS 1 HINDI</td>
+    <td>Star Sports is a major broadcaster of cricket in India, holding the pay television rights to domestic national team matches, the Indian Premier League, and ...</td>
+    <td><a href = "sportsplayer.php?v=starhindi">Play Link</a></td>
+  </tr>
+  <tr>
+    <td>PTV SPORTS</td>
+    <td>PTV Sports HD is a 24-hour sports channel owned by the Pakistan Television Corporation, Pakistan's state broadcaster.</td>
+    <td><a href = "sportsplayer.php?v=ptvpk">Play Link</a></td>
+  </tr>
+  <tr>
+    <td>WILLOW CRICKET (US)</td>
+    <td>WILLOW (Willow Cricket, Willow Extra and Willow Canada) is an American pay television sports channel which is completely devoted to airing overseas cricket events...</td>
+    <td><a href = "sportsplayer.php?v=willowusa">Play Link</a></td>
+  </tr>
+  <tr>
+    <td>SUPER SPORTS CRICKET</td>
+    <td>The channel broadcasts most of the major sporting events and leagues of association football, rugby, cricket, tennis, golf, motorsport, cycling, boxing, ...</td>
+    <td><a href = "sportsplayer.php?v=supercricket">Play Link</a></td>
+  </tr>
+  <tr>
+    <td>TEN SPORTS</td>
+    <td>Ten Sports HD is one of the most popular sports channel in Asia. It has broadcasting rights for many sporting events, including Cricket, Football, MMA and Tennis.</td>
+    <td><a href = "sportsplayer.php?v=tenspk">Play Link</a></td>
+  </tr>
+  <tr>
+    <td>WILLOW EXTRA</td>
+    <td>Indian Version of WILLOW CRICKET</td>
+    <td><a href = "sportsplayer.php?v=willowextra">Play Link</a></td>
+  </tr>
+  <tr>
+    <td>SKY SPORTS PREMIYM</td>
+    <td>Sky Sports Main Event, Premier League, Football, Cricket, Golf, F1, Action and Arena are available as a premium package on top of the basic Sky package.</td>
+    <td><a href = "sportsplayer.php?v=skysprem">Play Link</a></td>
+  </tr>
+  <tr>
+    <td>A SPORTS</td>
+    <td>A Sports HD is a Pakistani sports channel. It was launched on 16 October 2021 and is a part of ARY Digital Network which earlier planned in 2016.</td>
+    <td><a href = "sportsplayer.php?v=asportshd">Play Link</a></td>
+  </tr>
+  <tr>
+    <td>FOX 501 CRICKET</td>
+    <td>Fox Cricket is an Australian subscription television channel dedicated to screening cricket (both domestic and international) matches and related programming.</td>
+    <td><a href = "sportsplayer.php?v=fox501">Play Link</a></td>
+  </tr>
+  <tr>
+    <td>SKY SPORTS FOOTBALL</td>
+    <td>Sky Sports Football - Live games, scores, latest football news, transfers, results, fixtures and team news from the Premier to the Champions League.</td>
+    <td><a href = "sportsplayer.php?v=skysfott">Play Link</a></td>
+  </tr>
+  <tr>
+    <td>LALIGA UM</td>
+    <td>All the information of LaLiga Santander, LaLiga SmartBank, and Liga F: schedules, results, standings, news, statistics, and much more.</td>
+    <td><a href = "sportsplayer.php?v=laligauk">Play Link</a></td>
+  </tr>
+  <tr>
+    <td>SUPER SPORTS PREMIER LEAGUE</td>
+    <td>SuperSport delivers comprehensive coverage of major sporting events, including video highlights, results, fixtures, logs, news, TV schedules and more.</td>
+    <td><a href = "sportsplayer.php?v=superpremier">Play Link</a></td>
+  </tr>
+  <tr>
+    <td>SONY SIX</td>
+    <td>Sony Sports Ten 5 (formerly Sony Six) is an Indian English-language pay television sports channel operated by Culver Max Entertainment</td>
+    <td><a href = "sportsplayer.php?v=jvjmt31JC920190929">Play Link</a></td>
+  </tr>
+    <td>SONY ESPN</td>
+    <td>Sony ESPN was an Indian pay television sports channel which was a joint venture between ESPN Inc. and Sony Pictures Networks India</td>
+    <td><a href = "sportsplayer.php?v=YNuzz6acZy20190929">Play Link</a></td>
+  </tr>
+
+
+</body>
+</html>
+
+
